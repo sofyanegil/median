ca27a27cc5053a6c29fee6c30cfe4823240b8626
Sofyan Egi <sofyanegil@gmail.com>
2025-01-23 14:35:16 +0700

    feat(articles): implement CRUD operations for articles with DTOs and Swagger integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { ArticlesModule } from './articles/articles.module';
 
 @Module({
-  imports: [PrismaModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PrismaModule, ArticlesModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
