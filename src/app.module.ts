ccbfc167e055d5ba5807d172ed76bc799eb83c21
Sofyan Egi <sofyanegil@gmail.com>
2025-01-23 16:42:39 +0700

    feat(auth): implement authentication module with login functionality and JWT support

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { ArticlesModule } from './articles/articles.module';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PrismaModule, ArticlesModule, UsersModule],
+  imports: [PrismaModule, ArticlesModule, UsersModule, AuthModule],
   controllers: [],
   providers: [],
 })
